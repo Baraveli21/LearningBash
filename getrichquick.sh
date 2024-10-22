514212c1d241d9562553711c9cd95a51d9fc8616
Baraveli21 <baraveliryuo@gmail.com>
2024-10-23 02:30:48 +0500

    My commit mssaage

--- getrichquick.sh
+++ getrichquick.sh
@@ -4,5 +4,6 @@ echo "Name thyself: "
 read name
 echo "State thine age !!: "
 read age
-
+read -p "What is your favourite cake name: " cake
 echo "Hello $name, you are $age years old"
+echo "Seems you really like $cake"
